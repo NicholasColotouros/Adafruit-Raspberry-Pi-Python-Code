cd8445b411bcea9292c647ffaf5ee4ba775355d3
Nicholas Colotouros <agentnnc@gmail.com>
2014-08-19 19:48:52 +0000

    Fixed cursor movement bug during menu change

--- RaspiCalc.py
+++ RaspiCalc.py
@@ -36,9 +36,19 @@ def update(menuChanged):
 
     if menuChanged:
         lcd.setCursor(0,1)
+        cursorLocation = 0
     else:
         lcd.setCursor(cursorLocation,1)
 
+"""
+Sends an error message to the display then updates the screen
+"""
+def errorMessage(errMessage):
+    lcd.blink()
+    lcd.clear()
+    lcd.message(errMessage)
+    sleep(4)
+    update(False)
 
 # Startup
 lcd.blink()
@@ -70,8 +80,37 @@ while True:
     elif lcd.buttonPressed(lcd.UP):
         showNumbers = not showNumbers
         update(True)
+        sleep(delayTime)
 
     # Delete last entry
     elif lcd.buttonPressed(lcd.DOWN):
         input = input[:-1]
         update(False)
+        sleep(delayTime)
+
+    # Select the current entry
+    elif lcd.buttonPressed(lcd.SELECT):
+
+        # If = was selected, evaluate
+        if not showNumbers and symbols[cursorLocation] == '=':
+            try:
+                result = eval(input)
+
+                # If the result can't fit in the screen, overflow error
+                if result > 10^16:
+                    errorMessage("Overflow Error")
+                else:
+                    input = result
+                    update(False)
+
+            except:
+                errorMessage("Invalid Syntax")
+
+        # Something else was selected
+        else:
+            if showNumbers:
+                input = input + numbers[cursorLocation]
+            else:
+                input = input + symbols[cursorLocation]
+            update(False)
+        sleep(delayTime)    

eb89a17664ea2adefd277c83cb00acc457668b19
Nicholas Colotouros <agentnnc@gmail.com>
2014-08-23 20:14:56 +0000

    Fixed cursor movement bug

--- RaspiCalc.py
+++ RaspiCalc.py
@@ -26,20 +26,15 @@ delayTime = 1         # The time it takes to look for another button press
 Updates the display based on the global variables.
 
 MenuChanged signals a change from numbers to symbols or vice versa.
-CursorLocation needs to be passed in order for python to update the variable.
 """
-def update(menuChanged, cursorLocation):
+def update():
     lcd.clear()
     if showNumbers:
         lcd.message(input+"\n"+numbers)
     else:
         lcd.message(input+"\n"+symbols)
 
-    if menuChanged:
-        lcd.setCursor(0,1)
-        cursorLocation = 0
-    else:
-        lcd.setCursor(cursorLocation,1)
+    lcd.setCursor(cursorLocation,1)
 
 """
 Sends an error message to the display then updates the screen
@@ -49,11 +44,11 @@ def errorMessage(errMessage):
     lcd.clear()
     lcd.message(errMessage)
     sleep(4)
-    update(False, cursorLocation)
+    update()
 
 # Startup
 lcd.blink()
-update(False, cursorLocation)
+update()
 
 
 #Polls for input, sleep for 2 seconds after every press to ensure only 1 action per press
@@ -61,9 +56,10 @@ while True:
     # Move left
     if lcd.buttonPressed(lcd.LEFT):
         if cursorLocation > 0:
-            cursorLocation = cursorLocation - 1
+            cursorLocation -= 1
             lcd.setCursor(cursorLocation, 1)
-        sleep(delayTime)
+
+            sleep(delayTime)
          
     # Move right
     elif lcd.buttonPressed(lcd.RIGHT):
@@ -80,13 +76,14 @@ while True:
     # Toggle display
     elif lcd.buttonPressed(lcd.UP):
         showNumbers = not showNumbers
-        update(True, cursorLocation)
+        cursorLocation = 0
+        update()
         sleep(delayTime)
 
     # Delete last entry
     elif lcd.buttonPressed(lcd.DOWN):
         input = input[:-1]
-        update(False, cursorLocation)
+        update()
         sleep(delayTime)
 
     # Select the current entry
@@ -99,13 +96,13 @@ while True:
                 result = str(input)
 
                 # If the result can't fit in the screen, overflow error
-                if result >= 10^16:
+                if int(result) >= 10^16:
                     errorMessage("Overflow Error")
                 else:
                     input = result
-                    update(False, cursorLocation)
+                    update()
 
-            except:
+            except SyntaxError:
                 errorMessage("Invalid Syntax")
                 print input
 
@@ -115,5 +112,5 @@ while True:
                 input = input + numbers[cursorLocation]
             else:
                 input = input + symbols[cursorLocation]
-            update(False, cursorLocation)
+            update()
         sleep(delayTime)    
